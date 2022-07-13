71053c394198ceb3e8c06916f8e596971135bf17
EC2 Default User <ec2-user@ip-172-31-5-55.ap-northeast-1.compute.internal>
2022-07-13 13:14:20 +0000

    非同期通信修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,22 +3,16 @@ class FavoritesController < ApplicationController
   
   
   def create
-    Favorite.create(favorite_params)
     @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+    # app/views/favorites/create.js.erbを参照する
   end
 
   def destroy
-    if favorite = Favorite.find_by(favorite_params)
-      favorite.destroy
-    end
     @book = Book.find(params[:book_id])
-  end  
-  
-  private
-  
-  def favorite_params
-    { book_id: params[:book_id], user_id: current_user.id }
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    # app/views/favorites/destroy.js.erbを参照する
   end
-  
-  
 end
